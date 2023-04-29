1ba13a7339401f2167de10a30a7035b5906d6524
jaswanthakhil <jaswanth.akhil@vervenest.in>
2023-04-29 18:15:44 +0530

    sortbyAlpha

---
+++ Preparation/sortbyalpha.js
@@ -0,0 +1,7 @@
+const completeWord= "react js is a library"
+
+const divTowords= completeWord.split(' ')
+console.log(divTowords)
+const sortbyAlpha=divTowords.sort()
+console.log(divTowords)
+console.log(sortbyAlpha)
